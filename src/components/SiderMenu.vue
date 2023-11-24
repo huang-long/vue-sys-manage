2fc3e69cc7f75903d49ef8549b70ee472822e91a
huanglong <huanglong@163.com>
2023-11-24 14:39:29 +0800

    echarts优化

--- src/components/SiderMenu.vue
+++ src/components/SiderMenu.vue
@@ -1,6 +1,6 @@
 
 <script lang="ts">
-import { ref, computed, watch} from "vue";
+import { ref, computed, watch } from "vue";
 import { userStore } from "../stores/counter";
 import { useRoute, useRouter } from "vue-router";
 
@@ -45,7 +45,7 @@ export default {
 <template>
   <Sider ref="sideMenu" hide-trigger collapsible :collapsed-width="78" :width="220" v-model="isCollapsed" class="sider">
 
-    <Menu :theme="menuTheme" width="auto" :class="menuitemClasses" accordion>
+    <Menu :theme="'light'" width="auto" :class="menuitemClasses" accordion>
       <template v-for="item in menu">
         <!-- 有二级子菜单 -->
         <Submenu v-if="item.children && item.children.length" :key="'1_' + item.id" :name="item.id">
@@ -64,7 +64,7 @@ export default {
                     <Dropdown v-if="item2.children && item2.children.length" :key="item2.id" placement="right-start">
                       <DropdownItem>
                         <div class="ivu-menu-item">
-                          {{item2.title}}
+                          {{ item2.title }}
                           <Icon type="ios-arrow-forward"></Icon>
                         </div>
                       </DropdownItem>
@@ -78,7 +78,7 @@ export default {
                     </Dropdown>
 
                     <!-- 无三级子菜单 -->
-                    <DropdownItem v-else :key="item2.id">
+                    <DropdownItem v-else :key="item2.id + '2'">
                       <MenuItem :name="item2.id" :to="item2.path">{{ item2.title }}</MenuItem>
                     </DropdownItem>
                   </template>
@@ -102,7 +102,7 @@ export default {
               </Submenu>
 
               <!-- 无三级子菜单 -->
-              <MenuItem v-else :key="item2.id" :name="item2.path" :to="item2.path">
+              <MenuItem v-else :key="item2.id + '2'" :name="item2.path" :to="item2.path">
               <span>{{ item2.title }}</span>
               </MenuItem>
             </template>
@@ -110,7 +110,7 @@ export default {
         </Submenu>
 
         <!-- 没有子菜单 -->
-        <MenuItem v-else :name="item.id" :to="item.path" :key="'2_'+ item.id">
+        <MenuItem v-else :name="item.id" :to="item.path" :key="'2_' + item.id">
         <Icon :type="item.icon" />
         <span>{{ item.title }}</span>
         <Tooltip v-if="isCollapsed" :content="item.title" placement="right">
@@ -136,7 +136,7 @@ export default {
   width: 100%;
   height: 100%;
 
- :deep(.ivu-tooltip-rel) {
+  :deep(.ivu-tooltip-rel) {
     width: 100%;
     height: 100%;
   }
@@ -158,6 +158,7 @@ export default {
     height: 100%;
   }
 }
+
 .menu-item {
   height: 100%;
 
@@ -170,6 +171,7 @@ export default {
     vertical-align: bottom;
     transition: width 0.2s ease 0.2s;
   }
+
   i {
     transform: translateX(0px);
     transition: font-size 0.2s ease, transform 0.2s ease;
@@ -189,6 +191,7 @@ export default {
     width: 0px;
     transition: width 0.2s ease;
   }
+
   i {
     transform: translateX(5px);
     transition: font-size 0.2s ease 0.2s, transform 0.2s ease 0.2s;
